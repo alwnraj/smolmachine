8fde02c17230c5f66c5a97a73faed56fe9df6609
Alwin Rajkumar <141786788+alwnraj@users.noreply.github.com>
2025-01-08 17:19:10 -0500

    intial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # smolmachine
-This is a small teachable machine made using ml5.js
+Machine learing on the browser! Used a pre-trained model(mobilenet) to classify images and teach the machine to differentiate between two sets of images. 
+
+Tip: the more images you use, the better it identifies the images properly
---
+++ script.js
@@ -0,0 +1,15 @@
+let fileInput = document.getElementById('fileInput')
+let imageContainer = document.getElementById('imageContainer')
+let message  = document.getElementById('message, ')
+let labelInput = document.getElementById('labelInput')
+let addButton = document.getElementById('addButton')
+let trainButton = document.getElementById('trainButton')
+let predictButton = document.getElementById('predictButton')
+
+let featureExtractor;
+let classifier;
+let uploadImages = [];
+
+//modal loaded
+//adding images
+//prediction
